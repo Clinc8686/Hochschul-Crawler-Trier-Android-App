d0b6bcae342aecb14b7271d37c721adfa496093a
Mario <lampertm@hochschule-trier.de>
2022-01-29 21:56:52 +0100

    Added on boot start function

---
+++ app/src/main/java/de/clinc8686/hochschul_crawler/BootCompletedReceiver.java
@@ -0,0 +1,25 @@
+package de.clinc8686.hochschul_crawler;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    @SuppressLint({"BatteryLife", "UnsafeProtectedBroadcastReceiver"})
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences prefs = context.getSharedPreferences(context.getResources().getString(R.string.app_name), Context.MODE_PRIVATE);
+        int interval = prefs.getInt("interval", 0);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(context, Crawler_Service.class);
+        i.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+        @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pi = PendingIntent.getBroadcast(context, 8686, i, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pi);
+    }
+}
