9abc0a7b9d8af97c961e391aa1cec84825b08117
Mario <lampertm@hochschule-trier.de>
2022-02-03 23:19:05 +0100

    Added Alarm class to create Alarms

---
+++ app/src/main/java/de/clinc8686/hochschul_crawler/Alarm.java
@@ -0,0 +1,55 @@
+package de.clinc8686.hochschul_crawler;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.PowerManager;
+import android.provider.Settings;
+
+@SuppressLint("BatteryLife")
+public class Alarm {
+    private  Context context;
+
+    Alarm(Context context, int value) {
+        this.context = context;
+
+        PowerManager powerManager = (PowerManager) this.context.getSystemService(Context.POWER_SERVICE);
+        Intent intentFlag = new Intent();
+        intentFlag.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        if (powerManager.isIgnoringBatteryOptimizations(context.getPackageName())) {
+            intentFlag.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+        } else {
+            intentFlag.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intentFlag.setData(Uri.parse("package:" + context.getPackageName()));
+            context.startActivity(intentFlag);
+        }
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intentCrawlerClass = new Intent(context, Crawler_Service.class);
+        intentCrawlerClass.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+        @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pi = PendingIntent.getBroadcast(context, 8686, intentCrawlerClass, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), (value * 60) * 1000, pi);
+    }
+
+    public static void stopAlarm(Context context) {
+        Intent intent = new Intent(context, Crawler_Service.class).setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+        @SuppressLint("UnspecifiedImmutableFlag") PendingIntent sender = PendingIntent.getBroadcast(context, 8686, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(sender);
+        sender.cancel();
+    }
+
+    public static boolean checkAlarm(Context context) {
+        Intent intent = new Intent(context, Crawler_Service.class).setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+        @SuppressLint("UnspecifiedImmutableFlag") boolean alarmUp = PendingIntent.getBroadcast(context, 8686, intent, PendingIntent.FLAG_NO_CREATE) != null;
+        return alarmUp;
+    }
+
+    public static boolean checkIntent(Context context) {
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        return powerManager.isIgnoringBatteryOptimizations(context.getPackageName());
+    }
+}
