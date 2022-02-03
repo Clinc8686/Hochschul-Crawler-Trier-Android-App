e21aea597c8b05a50c565dcf0fabb9b8134830fd
Mario <lampertm@hochschule-trier.de>
2022-02-03 23:08:52 +0100

    removed unused constructor

--- app/src/main/java/de/clinc8686/hochschul_crawler/TooManyFalseLoginException.java
+++ app/src/main/java/de/clinc8686/hochschul_crawler/TooManyFalseLoginException.java
@@ -1,8 +1,6 @@
 package de.clinc8686.hochschul_crawler;
 
 public class TooManyFalseLoginException extends Exception {
-    TooManyFalseLoginException() {};
-
     public TooManyFalseLoginException(String message) {
         super(message);
     }
