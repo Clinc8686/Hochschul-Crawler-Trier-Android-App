530b756141f53c905429c7367492a418111bfa8e
Mario <lampertm@hochschule-trier.de>
2022-02-03 23:22:28 +0100

    added ViewPageAdapter

---
+++ app/src/main/java/de/clinc8686/hochschul_crawler/ViewPageAdapter.java
@@ -0,0 +1,31 @@
+package de.clinc8686.hochschul_crawler;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPageAdapter extends FragmentStateAdapter  {
+    private ArrayList<Fragment> fragments;
+
+    public ViewPageAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+
+    public void setData(ArrayList<Fragment> fragments) {
+        this.fragments = fragments;
+    }
+}
